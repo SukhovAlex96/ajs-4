10ea30a677d8d2e03b00d65b687773cdb267d7fb
Alex_Sukhov <Alekc-suhov@yandex.ru>
2023-10-01 10:37:17 +0300

    homework

--- src/js/_tests_/basic.test.js
+++ src/js/_tests_/basic.test.js
@@ -1,13 +1,13 @@
 import healthIndicator from '../basic';
 
 test.each([
-    [{ name: 'Маг', health: 90 }, 'healthy'],
-    [{ name: 'Маг', health: 40 }, 'wounder'],
-    [{ name: 'Маг', health: 10 }, 'critical'],
+  [{ name: 'Маг', health: 90 }, 'healthy'],
+  [{ name: 'Маг', health: 40 }, 'wounder'],
+  [{ name: 'Маг', health: 10 }, 'critical'],
 ])(
-    ('should show health string'),
-    (input, expected) => {
-        const result = healthIndicator(input);
-        expect(result).toBe(expected);
-    },
+  ('should show health string'),
+  (input, expected) => {
+    const result = healthIndicator(input);
+    expect(result).toBe(expected);
+  },
 );
--- src/js/_tests_/user.test.js
+++ src/js/_tests_/user.test.js
@@ -4,13 +4,13 @@ import { httpGet } from '../http';
 jest.mock('../http');
 
 beforeEach(() => {
-    jest.resetAllMocks();
+  jest.resetAllMocks();
 });
 
 test('should call loadUser once', () => {
-    httpGet.mockReturnValue(JSON.stringify({}));
+  httpGet.mockReturnValue(JSON.stringify({}));
 
-    const response = loadUser(1);
-    expect(response).toEqual({});
-    expect(httpGet).toBeCalledWith('http://server:8080/users/1');
+  const response = loadUser(1);
+  expect(response).toEqual({});
+  expect(httpGet).toBeCalledWith('http://server:8080/users/1');
 });
